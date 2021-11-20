d5837e2cb20738d415ffd76b0873dfa015cd69ee
guebebraga <guebe.braga5@gmail.com>
2021-11-20 14:33:39 -0300

    loginYregistro

--- src/scripts/miCuenta.js
+++ src/scripts/miCuenta.js
@@ -1,11 +1,16 @@
-const boton = document.getElementById("crear");
-
+//tomo elementos del form crear cuenta
+const botonCrear = document.getElementById("crear");
 const mailElemento= document.getElementById("mail");
 const nameElemento = document.getElementById("name");
 const passElemento = document.getElementById("password");
 
+// tomo elementod del form login
+const botonLogin = document.getElementById("login");
+const mail2Elemento = document.getElementById("mail2");
+const pass2Elemento = document.getElementById("pass");
 
-boton.addEventListener("click",()=>{
+
+botonCrear.addEventListener("click",()=>{
     const mail = mailElemento.value;
     const name = nameElemento.value;
     const pass = passElemento.value;
@@ -25,9 +30,39 @@ boton.addEventListener("click",()=>{
             body: JSON.stringify(objetoBody),
         })
         .then(response => response.json())
-        .then(data => alert(" Resultado => " + data.message));
+        .then(data => alert(data.message));
     }
     else{
-        alert("Falta completar algun campo")
+        alert("Debe completar todos los campos")
     }
+});
+
+botonLogin.addEventListener("click",()=>{
+    const mail2 = mail2Elemento.value
+    const pass2 = pass2Elemento.value
+
+    if(mail2 && pass2){
+        const objBody ={
+            mail : mail2,
+            password: pass2
+            
+        };
+        fetch("http://localhost:4000/miCuenta/login" , {
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(objBody),
+        })
+        .then(response => response.json())
+        .then(data => alert(data.message));
+        
+        if(!data.error){
+         localStorage.setItem("token", response.token);
+        }
+    }
+    else {
+        alert('Debe completar todos los campos')
+    }
+
 });
